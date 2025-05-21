fbf363accb260c4752bf5c66af87b779d5cbe567
MikeWu597 <i@hyp.ink>
2025-05-21 10:44:19 +0800

    Startup

--- src/main/java/cn/org/agatha/leaderboardLLM/LeaderboardLLM.java
+++ src/main/java/cn/org/agatha/leaderboardLLM/LeaderboardLLM.java
@@ -1,5 +1,6 @@
 package cn.org.agatha.leaderboardLLM;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LeaderboardLLM extends JavaPlugin {
@@ -8,6 +9,15 @@ public final class LeaderboardLLM extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        // 加载配置文件
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+
+        // 检查配置文件中是否包含 APP_ID 和 dashscope key
+        FileConfiguration config = getConfig();
+        if (!config.isSet("app_id") || !config.isSet("dashscope_key")) {
+            getLogger().warning("config.yml 中缺少 app_id 或 dashscope_key，请检查配置文件！");
+        }
     }
 
     @Override
