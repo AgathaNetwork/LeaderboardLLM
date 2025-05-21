3ac7e39ad4890d37db610a5fa145057b04763e57
MikeWu597 <i@hyp.ink>
2025-05-21 10:24:47 +0800

    Startup

---
+++ src/main/java/cn/org/agatha/leaderboardLLM/LeaderboardLLM.java
@@ -0,0 +1,17 @@
+package cn.org.agatha.leaderboardLLM;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LeaderboardLLM extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
